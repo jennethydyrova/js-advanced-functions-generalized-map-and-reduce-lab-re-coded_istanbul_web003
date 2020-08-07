30f3e47a3ea2864911b060a7aa79d6c266949293
jennethydyrova <jennethydyrovaa@gmail.com>
2020-08-07 22:11:30 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Add your functions here
+const map = (array, func) => {
+  return array.map((element) => func(element));
+};
+
+const reduce = (array, func, stratingPoint) => {
+  if (!!stratingPoint == false) {
+    stratingPoint = array.shift();
+  }
+  return array.reduce(func, stratingPoint);
+};
